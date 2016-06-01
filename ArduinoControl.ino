a93a4acec6a72c335a5ec38afb6c14cf5e7f82cb
ckdgus1878 <rose1878@naver.com>
2016-06-02 01:20:38 +0900

    블루트스 아두이노기반 rc카 수동조작
    블루트스 연결 
    아두이노기반 rc카 수동조작 완료

--- ArduinoControl.ino
+++ ArduinoControl.ino
@@ -1,38 +1,43 @@
-#include <MeOrion.h>
-#include <Arduino.h>
-#include <SoftwareSerial.h>
-#include <Wire.h>
+// 소프트웨어 시리얼을 이용해서 블루투스 초기세팅하는 소스
+#include <SoftwareSerial.h> 
+SoftwareSerial BT(2, 3); //Connect HC-06 TX,RX 
 
+//모터A 컨트롤
+int IN1=7;
+int IN2=6;
+//모터B 컨트롤
+int IN3=5;
+int IN4=4;
 
-MeBluetooth bluetooth(PORT_4);
-MeDCMotor motor_9(9);
-MeDCMotor motor_10(10);
+char blue; //블루투스 가져오는값
 
-char blue;
-
-
-void setup() {
+void setup()  
+{
   Serial.begin(115200);
-  bluetooth.begin(115200);
-  pinMode(13, OUTPUT);
-  Serial.println("Bluetooth Start!");
+  BT.begin(115200);//블루투스연결
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
 }
 
-void loop() {
-  //블루투스 값 가져옴
-    char inDat;
-    char outDat;
-    if(bluetooth.available())
-    {
-        inDat = bluetooth.read();
-        Serial.println(inDat);
-    }
-    if(Serial.available())
-    {
-        outDat = Serial.read();
-        bluetooth.write(outDat);
-    }
-
+void loop()
+{
+  char inDat;
+  char outDat;
+  
+  if (BT.available())
+  {
+    inDat = BT.read();
+    Serial.write(BT.read());
+  }
+  
+  if (Serial.available())
+  {
+    outDat = Serial.read();
+    BT.write(Serial.read());
+  }
+  
 //블루투스 가져온값을 아두이노 변수값을 동일하게만들어줌
     if(inDat =='e')
     blue ='e';
@@ -44,40 +49,94 @@ void loop() {
     blue ='s';
     else if(inDat =='d')
     blue ='d';
-
-    
+    else if(inDat =='q')
+    blue ='q';
 
     Serial.println(inDat);
     Serial.println(blue);
-    //motor_9.run(-50);
-    //motor_10.run(50);
 
     //직진
     if(blue == 'w'){
-    motor_9.run(-50);
-    motor_10.run(50);
+    motorA_Rotation();
     }
     //후진
     if(blue == 's'){
-    motor_9.run(50);
-    motor_10.run(-50);
+    motorA_Reverse();
     }
     //좌회전
     if(blue == 'a'){
-    motor_9.run(50);
-    motor_10.run(50);
+    motorB_Rotation();
     }
     //우회전
     if(blue == 'd'){
-    motor_9.run(-50);
-    motor_10.run(-50);
+    motorB_Reverse();
     }
     
     //정지
     if(blue == 'e'){
-    motor_9.run(0);
-    motor_10.run(0);
+    stopMotor();
+    }
+
+    //저속
+    if(blue == 'q'){
+    motorA_Rotation2();
     }
 
     delay(100);
+}
+
+//모터A,B Stop 정지
+void stopMotor()
+{
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,LOW);
+}
+ 
+ 
+//모터A 정회전, 모터B Stop 직진
+void motorA_Rotation()
+{
+    digitalWrite(IN1,HIGH);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,LOW);
+}
+//모터A 정회전, 모터B Stop 속도제어 test 
+void motorA_Rotation2()
+{
+    digitalWrite(IN1,HIGH);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,LOW);
+    analogWrite(8, 20); //0~255
+    analogWrite(9, 20); //0~255
+    Serial.println("speed");
+}
+//모터A 역회전, 모터B Stop 후진
+void motorA_Reverse()
+{
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,HIGH);
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,LOW);
+}
+ 
+//모터A Stop, 모터B 정회전  좌회전                                                  
+void motorB_Rotation()
+{
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,HIGH);
+    digitalWrite(IN4,LOW);
+}
+ 
+//모터A Stop, 모터B 역회전 우회전
+void motorB_Reverse()
+{
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,HIGH);
 }
