c9061a45ebd81bc8eb9da6e70b20e7a732697450
geolgeol <ingeol001@gmail.com>
2016-06-10 21:50:09 +0900

    선 인식
    조금더 보완해야됨

--- houghline.py
+++ houghline.py
@@ -1,44 +1,26 @@
-#!/usr/bin/python
-'''
-This example illustrates how to use Hough Transform to find lines
-Usage: ./houghlines.py [<image_name>]
-image argument defaults to ../data/pic1.png
-'''
-import cv2
 import numpy as np
-import sys
-import math
+import cv2
 
-try:
-    fn = sys.argv[1]
-except:
-    fn = "/home/pi/report/test1.jpg"
-print __doc__
-src = cv2.imread(fn)
-dst = cv2.Canny(src, 50, 200)
-cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+cam = cv2.VideoCapture(0)
 
-if True: # HoughLinesP
-    lines = cv2.HoughLinesP(dst, 1, math.pi/180.0, 40, np.array([]), 50, 10)
-    a,b,c = lines.shape
-    for i in range(a):
-        cv2.line(cdst, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 0, 2, 0, 255), 3, cv2.LINE_AA)
-else:    # HoughLines
-    lines = cv2.HoughLines(dst, 1, math.pi/180.0, 50, np.array([]), 0, 0)
-    a,b,c = lines.shape
-    for i in range(a):
-        rho = lines[i][0][0]
-        theta = lines[i][0][1]
-        a = math.cos(theta)
-        b = math.sin(theta)
-        x0, y0 = a*rho, b*rho
-        pt1 = ( int(x0+1000*(-b)), int(y0+1000*(a)) )
-        pt2 = ( int(x0-1000*(-b)), int(y0-1000*(a)) )
-        cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
-cv2.imshow("source", src)
-cv2.imshow("detected lines", cdst)
-cv2.waitKey(0)
+while (True):
 
+    s, img = cam.read()
 
+    edges = cv2.Canny(img, 100, 200)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180 , 40, None, 50, 10)
 
+    if lines is not None:
+        for line in lines[0]:
+            pt1 = (line[0], line[1])
+            pt2 = (line[2], line[3])
 
+            cv2.line(img, pt1, pt2, (0, 0, 255), 3)
+
+    cv2.imshow('edges', img)
+
+    if cv2.waitKey(10) & 0xff == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
