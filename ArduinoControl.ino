c4a08cfaad25e575de6ee5e47cf3162b3885ad3b
ckdgus1878 <rose1878@naver.com>
2016-06-12 20:38:58 +0900

    Update ArduinoControl.ino

--- ArduinoControl.ino
+++ ArduinoControl.ino
@@ -2,12 +2,10 @@
 #include <SoftwareSerial.h> 
 SoftwareSerial BT(2, 3); //Connect HC-06 TX,RX 
 
-//모터A 컨트롤
-int IN1=7;
-int IN2=6;
-//모터B 컨트롤
-int IN3=5;
-int IN4=4;
+int IN1=7; //뒷바퀴모터 +극을 7번핀에 연결
+int IN2=6; //뒷바퀴모터 -극을 6번핀에 연결
+int IN3=5; //앞바퀴모터 +극을 5번핀에 연결
+int IN4=4; //앞바퀴모터 -극을 4번핀에 연결
 
 char blue; //블루투스 가져오는값
 char inDat;//블루투스 가져온값을 넣는 변수
@@ -15,7 +13,7 @@ char outDat;
 
 void setup()  
 {
-  Serial.begin(115200);
+  Serial.begin(115200);//시리언모니터번호
   BT.begin(115200);//블루투스연결
   pinMode(IN1,OUTPUT); //뒷바퀴모터
   pinMode(IN2,OUTPUT); //뒷바퀴모터
@@ -30,8 +28,8 @@ void loop()
   
   if (BT.available())
   {
-    inDat = BT.read();
-    Serial.write(BT.read());
+    inDat = BT.read(); //블루투스에서 전송받은값을 inDat에 넣음
+    Serial.write(BT.read()); //가져온값 확인
   }
   
   if (Serial.available())
@@ -41,7 +39,7 @@ void loop()
   }
   
 //블루투스 가져온값을 아두이노 변수값을 동일하게만들어줌
-    if(inDat =='e')
+    if(inDat =='e') 
     blue ='e';
     else if(inDat =='w')
     blue ='w';
@@ -54,50 +52,39 @@ void loop()
     else if(inDat =='q')
     blue ='q';
 
-    Serial.println(inDat);
-    Serial.println(blue);
+    Serial.println(inDat); //동일하게 값만드는지 확인
+    Serial.println(blue); //쓰래기값이 나올때가있어서 수시로 동일하게 값을 만듬
 
     //직진
     if(blue == 'w'){
-    motorA_Rotation();
+    motorA_Rotation(); //블루투스값이 w이면 motorA_Rotation 실행
     }
     //후진
     if(blue == 's'){
-    motorA_Reverse();
+    motorA_Reverse(); //블루투스값이 s이면 motorA_Reverse 실행
     }
     //좌회전
     if(blue == 'a'){
-    motorB_Rotation();
+    motorB_Rotation(); //블루투스값이 a이면 motorB_Rotation 실행
     }
     //우회전
     if(blue == 'd'){
-    motorB_Reverse();
+    motorB_Reverse(); //블루투스값이 d이면 motorB_Reverse 실행
     }
     
     //정지
     if(blue == 'e'){
-    stopMotor();
+    stopMotor();    //블루투스값이 e이면 stopMotor 실행
     }
 
     //저속
     if(blue == 'q'){
     motorA_Rotation2();
     }
-
     delay(100);
-    
 }
 
-//모터A,B Stop 정지
-void stopMotor()
-{
-    digitalWrite(IN1,LOW);
-    digitalWrite(IN2,LOW);
-    digitalWrite(IN3,LOW);
-    digitalWrite(IN4,LOW);
-}
- 
- 
+
 //모터A 정회전, 모터B Stop 직진
 void motorA_Rotation()
 {
@@ -117,7 +104,6 @@ void motorA_Rotation2()
     digitalWrite(IN4,LOW);
     analogWrite(8, 100); //0~255
     analogWrite(9, 100); //0~255
-    Serial.println("speed");
 }
 //모터A 역회전, 모터B Stop 후진
 void motorA_Reverse()
@@ -150,4 +136,12 @@ void motorB_Reverse()
     digitalWrite(IN4,HIGH);
     analogWrite(8, 200); //0~255
     analogWrite(9, 200); //0~255
+}
+//모터A,B Stop 정지
+void stopMotor()
+{
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,LOW);
 }
